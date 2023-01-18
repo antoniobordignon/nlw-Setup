76f3bcd862be4dd768b97698c0f2f0452944be03
Antonio <antoniovictorbordignon@outlook.com>
2023-01-18 17:16:20 -0300

    created-autocomplete-days

--- web/src/components/summaryTable.tsx
+++ web/src/components/summaryTable.tsx
@@ -5,6 +5,9 @@ const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 
 const summaryDates = generateDatesFromYearBeginning()
 
+const minimumSummaryDatesSize = 18 * 7
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
 export function SummaryTable() {
     return (
         <div className="w-full flex">
@@ -24,6 +27,14 @@ export function SummaryTable() {
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(date => {
                     return <HabitDay key={date.toString()}/>
+                })}
+
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+                    return (
+                        <div 
+                        key={i} className="bg-zinc-900 w-10 h-10 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+                        />
+                    )
                 })}
             </div>
         </div>
