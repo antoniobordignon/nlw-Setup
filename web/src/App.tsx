f7bf9a35ccc1f12f529454e37222e0d73fc8ed82
Antonio <antoniovictorbordignon@outlook.com>
2023-01-18 16:15:15 -0300

    created-header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,14 @@
+import { Header } from './components/Header';
 import './styles/global.css';
 
-import Habit from "./components/Habit";
+// import Habit from "./components/Habit";
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={3}/>
-      <Habit completed={5}/>
-      <Habit completed={8}/>
-      <Habit completed={13}/>
-      <Habit completed={21}/>
-      <Habit completed={34}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
     </div>
   )
 }
-
-export default App
