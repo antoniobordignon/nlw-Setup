4f7f07ab4b29ccb3ea3136d78af207f45c20e365
Antonio <antoniovictorbordignon@outlook.com>
2023-01-19 16:31:34 -0300

    created-day-detail-popover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,11 +1,22 @@
+import * as Popover from '@radix-ui/react-popover'
+import { ProgressBar } from './ProgressBar';
+
 // interface habitDayProps {
 // completed: number
 //}
 
 export function HabitDay() {
     return (
-      <div className="bg-zinc-900 w-10 h-10 border-2 border-zinc-800 rounded-lg">
-        
-      </div>
+      <Popover.Root>
+        <Popover.Trigger className="bg-zinc-900 w-10 h-10 border-2 border-zinc-800 rounded-lg" />
+        <Popover.Portal>
+          <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col" >
+            <span className="font-semibold text-zinc-400">Thursday</span>
+            <span className="mt-1 font-extrabold leading-tight text-3xl">19/01</span>
+            <ProgressBar progress={75}/>
+            <Popover.Arrow height={8} width={16} className="fill-zinc-900"/>
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover.Root>
     );
   }
