d3e62f1efda848e0dd837a6acae0959353713bcd
Antonio <antoniovictorbordignon@outlook.com>
2023-01-22 20:54:37 -0300

    avoiding-editing-on-past-dates

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -11,6 +11,7 @@ import { ProgressBar } from "../components/ProgressBar";
 import { Checkbox } from "../components/Checkbox";
 import { Loading } from "../components/Loading";
 import { HabitsEmpty } from "../components/HabitsEmpty";
+import clsx from "clsx";
 
 
 interface Params {
@@ -34,6 +35,7 @@ export function Habit() {
     const { date } = route.params as Params;
 
     const parsedDate= dayjs(date);
+    const isDateInPast = parsedDate.endOf('day').isBefore(new Date());
     const dayOfWeek = parsedDate.format('dddd')
     const dayAndMonth = parsedDate.format('MM/DD')
 
@@ -94,7 +96,9 @@ export function Habit() {
                     progress={habitsProgress}
                 />
 
-                <View className="mt-6 ">
+                <View className={clsx("mt-6", {
+                    ["opacity-50"]: isDateInPast
+                })}>
                     {
                         dayInfo?.possibleHabits ?
                         dayInfo?.possibleHabits.map(habit => (
@@ -102,12 +106,20 @@ export function Habit() {
                             key={habit.id}
                             title={habit.title}
                             checked={completedHabits.includes(habit.id)}
+                            disabled={isDateInPast}
                             onPress={() => handleToggleHabit(habit.id)}
                         /> 
                         )) 
                         :<HabitsEmpty /> 
                     }
                 </View>
+                {
+                    isDateInPast && (
+                        <Text className="text-white mt-10 text-center">
+                            You cannot edit habits from a past date.
+                        </Text>
+                    )
+                }
             </ScrollView>
         </View>
     )
